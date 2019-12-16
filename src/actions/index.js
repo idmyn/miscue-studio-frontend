a8d9b2f243ff8c5939016c10a011ad180defa3ba
idmyn <hello@davidmyno.rs>
2019-12-16 15:50:02 +0000

    only dispatch next/previous word actions if they select a real word

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,20 @@ import API from "../adapters/API"
 
 export const selectNextWord = () => (dispatch, getState) => {
   const { selectedWordId, selectedStory } = getState()
+  const finalWordId = selectedStory.content[selectedStory.content.length - 1].id
   const nextWordId = selectedWordId
     ? selectedWordId + 1
     : selectedStory.content[0].id
-  dispatch({ type: "SET_SELECTED_WORD_ID", payload: { id: nextWordId } })
+  nextWordId <= finalWordId && dispatch({ type: "SET_SELECTED_WORD_ID", payload: { id: nextWordId } })
 }
 
 export const selectPreviousWord = () => (dispatch, getState) => {
   const { selectedWordId, selectedStory } = getState()
+  const firstWordId = selectedStory.content[0].id
   const previousWordId = selectedWordId
     ? selectedWordId - 1
-    : selectedStory.content[0].id
-  dispatch({ type: "SET_SELECTED_WORD_ID", payload: { id: previousWordId } })
+    : firstWordId
+  previousWordId >= firstWordId && dispatch({ type: "SET_SELECTED_WORD_ID", payload: { id: previousWordId } })
 }
 
 export const setSelectedStory = id => dispatch => {
