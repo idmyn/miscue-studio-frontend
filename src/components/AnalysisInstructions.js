812248a19cee091e02c756c70c56610ad733adcf
idmyn <hello@davidmyno.rs>
2019-12-31 10:16:04 +0000

    fill out AnalysisInstructions

--- src/components/AnalysisInstructions.js
+++ src/components/AnalysisInstructions.js
@@ -3,7 +3,14 @@ import React from "react"
 const AnalysisInstructions = () => {
   return (
     <div id="instructions">
-      Instructions
+      <p>
+        Imagine that studentName is reading storyName and making some
+        mistakes along the way. To mark a mistake on a word, click on the word
+        and select the category of mistake. Some mistakes are made with relevant
+        miscues. For example, if a student inserts a word that isn't in the
+        text, you can record that inserted word as a miscue.<br/ >
+        <strong>Happy marking!</strong>
+      </p>
     </div>
   )
 }
